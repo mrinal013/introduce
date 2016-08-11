94b7b406ec88660721d255b60e06b2290e360a54
mrinal13 <mrinalhaque99@gmail.com>
2016-08-11 12:41:48 +0600

    metabox v1

--- inc/metaboxes/metaboxes.php
+++ inc/metaboxes/metaboxes.php
@@ -7,9 +7,9 @@ function codestar_init(){
 }
 
 
-include_once get_template_directory() . '/inc/metaboxes/class/class.sectiontypes.php';
+require_once get_template_directory() . '/inc/metaboxes/class/class.sectiontypes.php';
 
-include_once get_template_directory() . '/inc/metaboxes/class/class.general.php';
+require_once get_template_directory() . '/inc/metaboxes/class/class.general.php';
 
 // call require class from metaboxes.php
 new SectionTypes();
@@ -29,37 +29,37 @@ function section_mb_filter(){
         }
         $section_type = $section_type_info['section_type'];
         if("banner"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.banner.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.banner.php';
 			new Banner();
         } else if("feature"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.feature.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.feature.php';
 			new Feature();
         } else if("portfolio"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.portfolio.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.portfolio.php';
 			new Portfolio();
         } else if("purchage"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.purchage.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.purchage.php';
 			new Purchage();
         } else if("summary"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.summary.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.summary.php';
 			new Summary();
         } else if("partner"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.partner.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.partner.php';
 			new Partner();
         } else if("skil"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.skil.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.skil.php';
 			new Skil();
         } else if("review"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.review.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.review.php';
 			new Review();
         } else if("team"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.team.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.team.php';
 			new Team();
         } else if("service"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.service.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.service.php';
 			new Service();
         } else if("blog"==$section_type){
-			include_once get_template_directory() . '/inc/metaboxes/class/class.blog.php';
+			require_once get_template_directory() . '/inc/metaboxes/class/class.blog.php';
 			new Blog();
         }
     }
