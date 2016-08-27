677b419d3a04e69e59d7a88066199f770db39d72
mrinal013 <mrinalhaque99@gmail.com>
2016-08-28 03:18:37 +0600

    js fix

--- js/one-page-nav.js
+++ js/one-page-nav.js
@@ -5,11 +5,5 @@
         slug = url[url.length - 2];
         slugid = '#' + slug;
         $(this).attr('href', slugid );
-<<<<<<< HEAD
-    });
-    
-=======
     })
-
->>>>>>> 04b1db41b297ae59d0ad7fe46f6cae7acb5450ae
 });
